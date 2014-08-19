6a2981319497debae7beaadc5501d7b7f282f3ec
Brian Chen <brianchen8990@gmail.com>
2014-08-19 22:47:14 +0200

    add basic date and time picker in the application

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -18,5 +18,59 @@ angular.module('booking')
         }
       };
 
-      $scope.eventSources = [];
+    $scope.eventSources = [];
+
+    $scope.today = function() {
+      $scope.bookingDate = new Date();
+      $scope.bookingTime = new Date();
+    };
+
+    $scope.clear = function () {
+      $scope.bookingDate = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+      $scope.minDate = $scope.minDate ? null : new Date();
+    };
+
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+    $scope.toggleMode = function() {
+      $scope.ismeridian = ! $scope.ismeridian;
+    };
+
+    $scope.update = function() {
+      var d = new Date();
+      d.setHours( 14 );
+      d.setMinutes( 0 );
+      $scope.mytime = d;
+    };
+
+    $scope.timeChanged = function () {
+      console.log('Time changed to: ' + $scope.bookingTime);
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.dateFormat = 'dd-MM-yyyy';
+
+    $scope.hstep = 1;
+    $scope.mstep = 5;
+    $scope.ismeridian = true;
+
+    $scope.today();
+    $scope.toggleMin();
   });
